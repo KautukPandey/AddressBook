08f4bc871bee2962611da2a1396ee3077234877e
Kautuk <kautukpandey2004@gmail.com>
2026-07-02 11:12:23 +0530

    [Kautuk] Add : UC1 creation

--- main.java
+++ main.java
@@ -1,5 +1,53 @@
-public class Main { 
-    public static void main(String[] args) {
-        System.err.println("Welcome to Address Book");
-    }
+import java.util.Scanner;
+
+class Contact{
+String firstName;
+String lastName;
+String address;
+String city;
+String state;
+String zip;
+String phone;
+String email;
+}
+
+public class Main{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+Contact c=new Contact();
+
+System.out.print("First Name: ");
+c.firstName=sc.nextLine();
+
+System.out.print("Last Name: ");
+c.lastName=sc.nextLine();
+
+System.out.print("Address: ");
+c.address=sc.nextLine();
+
+System.out.print("City: ");
+c.city=sc.nextLine();
+
+System.out.print("State: ");
+c.state=sc.nextLine();
+
+System.out.print("Zip: ");
+c.zip=sc.nextLine();
+
+System.out.print("Phone: ");
+c.phone=sc.nextLine();
+
+System.out.print("Email: ");
+c.email=sc.nextLine();
+
+System.out.println("\nContact Details");
+System.out.println("First Name : "+c.firstName);
+System.out.println("Last Name  : "+c.lastName);
+System.out.println("Address    : "+c.address);
+System.out.println("City       : "+c.city);
+System.out.println("State      : "+c.state);
+System.out.println("Zip        : "+c.zip);
+System.out.println("Phone      : "+c.phone);
+System.out.println("Email      : "+c.email);
+}
 }
